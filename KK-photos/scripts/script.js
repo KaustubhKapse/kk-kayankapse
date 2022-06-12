56911a54733d841997d66692e857bc9e9ea98ccb
kaustubh <88793836+KaustubhKapse@users.noreply.github.com>
2022-06-12 22:25:07 +0530

    Add files via upload

---
+++ KK-photos/scripts/script.js
@@ -0,0 +1,14 @@
+let elem = document.querySelector(".slider");
+
+
+document.querySelector('.slider').addEventListener('click', function () {
+  
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+    
+})
